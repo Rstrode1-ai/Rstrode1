70b7bae54348b18b6d75dbf9189f7c85a85dcdaf
Reginald D. Strode <ReginaldStrode@gmail.com>
2026-01-11 20:25:17 -0600

    calc.py

---
+++ calc.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Choose an operation (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    result = "Invalid operation"
+
+print("Result:", result)
