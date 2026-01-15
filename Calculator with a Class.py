27d4e738201e6debd225fcb03324cbcd3ec23c7c
Reginald D. Strode <ReginaldStrode@gmail.com>
2026-01-14 20:59:37 -0600

    Add Error Handling

--- Calculator with a Class.py
+++ Calculator with a Class.py
@@ -1,34 +1,5 @@
-class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b == 0:
-            return "Error: Division by zero"
-        return a / b
-calc = Calculator()
-
-while True:
-    num1 = float(input("Enter first number: ")) 
-    num2 = float(input("Enter second number: "))
-    operation = input("Enter operation (+,-,*,/):")
-
-    if operation == '+':
-        print(calc.add(num1, num2))
-    elif operation == '-':
-        print(calc.subtract(num1, num2))
-    elif operation == '*':
-        print(calc.multiply(num1, num2))
-    elif operation == '/':
-        print(calc.divide(num1, num2))
-    else:
-        print("Invalid operation")  
-
-    if input("Calculate again? (y/n): ").lower() != "y":
-        break
+try:
+    num1 =float(input("Enter first number: "))
+    num2 =float(input("Enter second number: ")) 
+except ValueError:
+    print("Please enter valid numbers")
